c4cd1e013e97d0fc83354084941b027653b0e8cb
Manoel Pereira dos Santos <manoelps@live.com>
2024-01-04 14:57:32 -0300

    atualizado rotas

--- start/routes.ts
+++ start/routes.ts
@@ -3,5 +3,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.resource('/farmers', 'FarmersController').apiOnly()
+    Route.delete('/farmers/:id/crops', 'FarmersController.destroyPlantedCrop')
+
+    Route.get('/crops', 'PlantedCropsController.index')
   }).prefix('/v1')
 }).prefix('/api')
