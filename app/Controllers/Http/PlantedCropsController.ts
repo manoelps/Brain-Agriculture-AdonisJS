570d686ec330127d20753153501bc7f31232e77d
Manoel Pereira dos Santos <manoelps@live.com>
2024-01-04 14:53:57 -0300

    Criado controlador dos tipos de culturas e mock dos dados mesmos

--- app/Controllers/Http/PlantedCropsController.ts
+++ app/Controllers/Http/PlantedCropsController.ts
@@ -1,30 +1,11 @@
+import PlantedCropsMock from 'App/Mocks/plantedCrops'
+
 export default class PlantedCropsController {
   public async index() {
-    const plantedCrops = [
-      {
-        id: 1,
-        cultura: 'Soja',
-      },
-      {
-        id: 2,
-        cultura: 'Milho',
-      },
-      {
-        id: 3,
-        cultura: 'Algodão',
-      },
-      {
-        id: 4,
-        cultura: 'Café',
-      },
-      {
-        id: 5,
-        cultura: 'Cana de Açucar',
-      },
-    ]
+    const plantedCrops = new PlantedCropsMock()
 
     return {
-      data: plantedCrops,
+      data: plantedCrops.mock_culture_crops,
     }
   }
 }
---
+++ app/Mocks/plantedCrops.ts
@@ -0,0 +1,24 @@
+export default class PlantedCropsMock {
+  public mock_culture_crops = [
+    {
+      id: 1,
+      cultura: 'Soja',
+    },
+    {
+      id: 2,
+      cultura: 'Milho',
+    },
+    {
+      id: 3,
+      cultura: 'Algodão',
+    },
+    {
+      id: 4,
+      cultura: 'Café',
+    },
+    {
+      id: 5,
+      cultura: 'Cana de Açucar',
+    },
+  ]
+}
